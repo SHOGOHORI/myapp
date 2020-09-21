558e78270e55da779437dc5aa1b1d91137ac6e96
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-09-21 17:51:52 +0900

    Create users model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'users/new'
   get  '/help',    to: 'static_pages#help'
   get  '/about',   to: 'static_pages#about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
