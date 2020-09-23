5d5c146af5ffe3d4b94628455e0831b8fa768619
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-09-23 20:55:19 +0900

    Add password validates

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -6,7 +6,7 @@ class UserTest < ActiveSupport::TestCase
   # end
   def setup
     @user = User.new(name: "Example User", email: "user@example.com",
-                     password: "foobar", password_confirmation: "foobar")
+                     password: "foobar1", password_confirmation: "foobar1")
   end
 
   test "should be valid" do
@@ -63,5 +63,20 @@ class UserTest < ActiveSupport::TestCase
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
+
+  test "password should be present (nonblank)" do
+    @user.password = @user.password_confirmation = " " * 6
+    assert_not @user.valid?
+  end
+
+  test "password should have a minimum length" do
+    @user.password = @user.password_confirmation = "a" * 5
+    assert_not @user.valid?
+  end
+
+  # test "パスワードに数字が含まれない場合無効な状態であること" do
+  #   @user.password = @user.password_confirmation = "a" * 7
+  #   assert_not @user.valid?
+  # end
 
 end
