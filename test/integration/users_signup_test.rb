9629491e39afa0d2ed1d2a6bda3ab6fc6ccb56b1
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-09-24 23:28:45 +0900

    Custom users_show

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -17,5 +17,18 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.alert'
   end
 
+  test "valid signup information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name:  "Example User",
+                                         email: "user@example.com",
+                                         password:              "Aaaaaaaa1?",
+                                         password_confirmation: "Aaaaaaaa1?" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
+
 
 end
