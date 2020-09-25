c75f7b83a44612b6dea4f9264a17b5572cbc5736
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-09-25 14:39:00 +0900

    Create login-from

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,17 @@
+class SessionsController < ApplicationController
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+    else
+      flash[:danger] = '無効なメールアドレスとパスワードの組合わせです'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
+end
