86487c0ad62d620d195d7e0232ea61078f4d71b0
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-09-25 17:58:39 +0900

    Custom about_page

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,7 +5,8 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
-      # ユーザーログイン後にユーザー情報のページにリダイレクトする
+      log_in user
+      redirect_to user
     else
       flash.now[:danger] = '無効なメールアドレスとパスワードの組合わせです'
       render 'new'
