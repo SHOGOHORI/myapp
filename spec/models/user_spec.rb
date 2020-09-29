941433780a507c418311a0a38b30ff58bd0f1592
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-09-29 18:14:08 +0900

    add user rspec

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,4 +4,24 @@ RSpec.describe User, type: :model do
   it '有効なユーザー' do
     expect(FactoryBot.create(:user)).to be_valid
   end
+  describe 'バリデーション' do
+    it { is_expected.to validate_presence_of(:name) }
+    it { is_expected.to validate_presence_of(:email) }
+    it { is_expected.to validate_length_of(:name).is_at_most(50) }
+    it { is_expected.to validate_length_of(:email).is_at_most(255) }
+    it '有効なメールアドレス' do
+      is_expected.to allow_values('first.last@foo.jp',
+                                  'user@example.com',
+                                  'USER@foo.COM',
+                                  'A_US-ER@foo.bar.org',
+                                  'alice+bob@baz.cn').for(:email)
+    end
+    it '無効なメールアドレス' do
+      is_expected.to_not allow_values('user@example,com',
+                                      'user_at_foo.org',
+                                      'user.name@example.',
+                                      'foo@bar_baz.com',
+                                      'foo@bar+baz.com').for(:email)
+    end
+  end
 end
