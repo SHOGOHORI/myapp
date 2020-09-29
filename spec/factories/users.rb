040f856367ee20b23935313f2ab9bffd9546e402
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-09-30 01:01:54 +0900

    create user spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :user do
-    sequence(:name) { |n| "User#{n}" }
-    sequence(:email) { |n| "tester#{n}@example.com" }
-    password { "Aaaaaaaa1?" }
-    password_confirmation { "Aaaaaaaa1?" }
+    name { "Example User" }
+    email { "user@example.com" }
+    password { 'Aaaaaaaa1?' }
+    password_confirmation { 'Aaaaaaaa1?' }
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,26 +2,109 @@ require 'rails_helper'
 
 RSpec.describe User, type: :model do
   it '有効なユーザー' do
-    expect(FactoryBot.create(:user)).to be_valid
+    expect( build(:user) ).to be_valid
   end
-  describe 'バリデーション' do
-    it { is_expected.to validate_presence_of(:name) }
-    it { is_expected.to validate_presence_of(:email) }
-    it { is_expected.to validate_length_of(:name).is_at_most(50) }
-    it { is_expected.to validate_length_of(:email).is_at_most(255) }
-    it '有効なメールアドレス' do
-      is_expected.to allow_values('first.last@foo.jp',
-                                  'user@example.com',
-                                  'USER@foo.COM',
-                                  'A_US-ER@foo.bar.org',
-                                  'alice+bob@baz.cn').for(:email)
+  # describe 'ユーザー名' do
+  #   let(:user) { build(:user, name: name ) }
+  #   context "ブランクの場合無効" do
+  #     name = "      "
+  #     it { is_expected.to_not be_valid }
+  #   end
+
+  #   context "長過ぎるの場合無効" do
+  #     name = "a" * 51
+  #     it { is_expected.to_not be_valid }
+  #   end
+  # end
+
+  describe 'メールアドレス' do
+    let(:user) { build(:user, email: email ) }
+    subject { user }
+    context "ブランクの場合無効" do
+      let(:email) { "       " }
+      it { is_expected.to_not be_valid }
     end
-    it '無効なメールアドレス' do
-      is_expected.to_not allow_values('user@example,com',
-                                      'user_at_foo.org',
-                                      'user.name@example.',
-                                      'foo@bar_baz.com',
-                                      'foo@bar+baz.com').for(:email)
+
+    context "長過ぎるの場合無効" do
+      let(:email) { "a" * 244 + "@example.com" }
+      it { is_expected.to_not be_valid }
+    end
+
+    context '有効なメールアドレス' do
+      valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
+                           first.last@foo.jp alice+bob@baz.cn]
+      valid_addresses.each do |valid_address|
+        let(:email) { valid_address }
+        it { is_expected.to_not be_valid }
+      end
+    end
+
+    context '無効なメールアドレス' do
+      invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
+                            foo@bar_baz.com foo@bar+baz.com foo@bar..com]
+      invalid_addresses.each do |invalid_address|
+        let(:email) { invalid_address }
+        it { is_expected.to_not be_valid }
+      end
+    end
+
+    describe '重複しているメールアドレス' do
+      let!(:user) { create(:user) }
+      it '重複しているメールアドレスは無効' do
+        user = build(:user, email: 'user@example.com')
+        expect(user).to_not be_valid
+      end
+    end
+
+    describe 'メールアドレスを小文字で保存する' do
+      let!(:user) { create(:user, email: 'ORIGINAL@EXAMPLE.COM') }
+      it 'メールアドレスを小文字で保存する' do
+        expect(user.reload.email).to eq 'original@example.com'
+      end
     end
   end
+
+  # describe "パスワード" do
+  #   let(:user) { build(:user, password: password, 
+  #                      password_confirmation: password) }
+  #   context "ブランクの場合無効" do
+  #     password = " " * 10
+  #     it { is_expected.to_not be_valid }
+  #   end
+
+  #   context "短すぎる場合無効" do
+  #     password = "a" * 6 + "A" + "1" + "?"
+  #     it { is_expected.to_not be_valid }
+  #   end
+
+  #   context "数字が含まれない場合無効" do
+  #     password = "a" * 8 + "A" + "?"
+  #     it { is_expected.to_not be_valid }
+  #   end
+
+  #   context "英大文字が含まれない場合無効" do
+  #     password = "a" * 8 + "1" + "?"
+  #     it { is_expected.to_not be_valid }
+  #   end
+
+  #   context "記号が含まれない場合無効" do
+  #     password = "a" * 8 + "A" + "1"
+  #     it { is_expected.to_not be_valid }
+  #   end
+
+  #   context "長すぎる場合無効" do
+  #     password = "a" * 38 + "A" + "1" + "?"
+  #     it { is_expected.to_not be_valid }
+  #   end
+  # end
+
+  # it "パスワードが一致しない" do
+  #   user = build(:user, password_confirmation: "Bbbbbbbb1?")
+  #   user.valid?
+  #   expect(user.errors[:password_confirmation]).to include("とパスワードの入力が一致しません")
+  # end
+
+  # it "authenticated?ダイジェストがnilのユーザーにはfalseを返す" do
+  #   it { is_expected.to eq true }
+  # end
 end
