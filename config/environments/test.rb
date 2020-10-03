244e60f71053ddb39865f0beb4c56fcc1ca3296c
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-10-03 23:00:23 +0900

    ユーザーメールのテスト作成

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -47,4 +47,7 @@ Rails.application.configure do
   # Raises error for missing translations.
   # config.action_view.raise_on_missing_translations = true
   config.middleware.use RackSessionAccess::Middleware
+
+  config.action_mailer.delivery_method = :test
+  config.action_mailer.default_url_options = { host: 'example.com' }
 end
--- spec/mailers/user_mailer_spec.rb
+++ spec/mailers/user_mailer_spec.rb
@@ -1,32 +1,20 @@
 require "rails_helper"
 
 RSpec.describe UserMailer, type: :mailer do
-  describe "account_activation" do
-    let(:mail) { UserMailer.account_activation }
-
-    it "renders the headers" do
-      expect(mail.subject).to eq("Account activation")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
+  let(:user) { create(:user) }
+  describe "アカウント有効化" do
+    let(:mail) { UserMailer.account_activation(user) }
+    it "ヘッダー" do
+      expect(mail.subject).to eq("【育児Q&A】アカウント認証手続きを完了してください")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["noreply@example.com"])
     end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
-    end
-  end
-
-  describe "password_reset" do
-    let(:mail) { UserMailer.password_reset }
-
-    it "renders the headers" do
-      expect(mail.subject).to eq("Password reset")
-      expect(mail.to).to eq(["to@example.org"])
-      expect(mail.from).to eq(["from@example.com"])
-    end
-
-    it "renders the body" do
-      expect(mail.body.encoded).to match("Hi")
+    # subject { mail.body.encoded }
+    it "ボディ" do
+      # expect(mail.body.encoded).to match("育児Q&Aに登録するには、以下のリンクをクリックしてアカウントを有効化してください。") 
+      expect(mail).to match(user.name)
+      expect(mail.body).to match(user.activation_token)
+      expect(mail.body).to match(CGI.escape(user.email))
     end
   end
-
 end
