6017f83d1313be4901c979af2ad1dca8df6ed462
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-10-06 00:03:57 +0900

    質問の順番をソート

--- spec/factories/questions.rb
+++ spec/factories/questions.rb
@@ -1,7 +1,8 @@
 FactoryBot.define do
   factory :question do
-    title { "title" }
-    content { "MyText" }
-    user_id { nil }
+    sequence(:title) { |n| "title#{n}" }
+    sequence(:content) { |n| "MyText#{n}" }
+    sequence(:user_id) { |n| }
+    sequence(:created_at) { Time.zone.now }
   end
 end
--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -6,21 +6,21 @@ RSpec.describe Question, type: :model do
     let(:question) { build(:question, title: title, content: content, user_id: user_id) }
 
     subject { question }
-    context "有効な質問" do
+    context '有効な質問' do
       let(:user_id) { user.id }
       let(:title) { "困っています" }
       let(:content) { "子供が泣き止みません" }
       it { is_expected.to be_valid }
     end
 
-    context "ユーザーID無し" do
+    context 'ユーザーID無し' do
       let(:user_id) { nil }
       let(:title) { "困っています" }
       let(:content) { "子供が泣き止みません" }
       it { is_expected.to_not be_valid }
     end
 
-    context "content無し" do
+    context 'content無し' do
       let(:user_id) { user.id }
       let(:title) { "   " }
       let(:content) { "子供が泣き止みません" }
@@ -34,11 +34,18 @@ RSpec.describe Question, type: :model do
       it { is_expected.to_not be_valid }
     end
 
-    context "titleが長すぎる" do
+    context 'titleが長すぎる' do
       let(:user_id) { user.id }
       let(:title) { "a" * 301 }
       let(:content) { "子供が泣き止みません" }
       it { is_expected.to_not be_valid }
+    end
+  end
+
+  describe '質問順' do
+    questions = create_list(:question, 40)
+    context '質問が最新順になっているか' do
+      it { expect(Question.first).to eq questions(:most_recent) }
     end
   end
 end
