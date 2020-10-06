2146cf804fd03ceca1f011f4b74ded6331c0ccd7
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-10-07 00:28:43 +0900

    ユーザー詳細画面作成

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -3,4 +3,8 @@ class QuestionsController < ApplicationController
   def index
     @questions = Question.paginate(page: params[:page])
   end
+
+  def show
+    @question = Question.find(params[:id])
+  end
 end
