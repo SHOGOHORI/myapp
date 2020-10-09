0b62a6601f1efc90bbca6e806cbdd6894670b775
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-10-09 22:40:07 +0900

    画像投稿機能追加

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -5,7 +5,7 @@ class Answer < ApplicationRecord
   validates :user_id, presence: true
   validates :question_id, presence: true
   validates :content, presence: true
-  has_many_attached :image
+  has_one_attached :image
   validates :image,   content_type: { in: %w[image/jpeg image/gif image/png],
                                       message: "有効な画像を投稿してください。" },
                                       size:         { less_than: 5.megabytes,
