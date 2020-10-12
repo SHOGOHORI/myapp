36b598af08196207b4c3b6b94df384456aa6cfc8
SHOGOHORI <shogo.hori.1988@gmail.com>
2020-10-13 00:56:11 +0900

    ページネーションAjax対応

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,6 +1,10 @@
 class StaticPagesController < ApplicationController
   def home
     @questions = Question.paginate(page: params[:page])
+    respond_to do |format|
+      format.html
+      format.js
+    end
   end
 
   def help
